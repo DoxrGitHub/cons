0d822bc9a6a2fae953610fc54215b34a49a3c788
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-10-01 16:25:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hey!')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi')
+})
+
+app.listen(3000, console.log('runnin dis shi'))
